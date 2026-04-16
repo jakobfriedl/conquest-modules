0fd6bf83b379f59ea74973f890e69111950be7c8
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2026-04-16 17:54:20 +0200

    Added basic 'bof-async' command.

---
+++ async/async.py
@@ -0,0 +1,23 @@
+import conquest 
+import os.path
+
+ASYNC_DLL = conquest.resources_root() + "/async-bof-loader/dist/async-bof.dll"
+EXPORT_FUNC = "Run"
+
+# Only register async commands if the DLL exists
+if not os.path.exists(ASYNC_DLL):
+    raise FileNotFoundError(f"Async BOF DLL not found: {ASYNC_DLL}")
+
+cmd_bofAsync = (
+    conquest.createCommand(name="bof-async", description="Execute an object file asynchronously in the background.", example="bof-async /path/to/process-notify.x64.o <packed-args>",
+                            message="Tasked agent to execute an object file asynchronously.", mitre=["T1055", "T1620"])
+            .addArgString("object-file", "Path to the object file to execute.", True)
+            .addArgString("arguments", "Arguments to be passed to the object file, packed as a HEX string according to beacon_generate.py.")
+            .setHandler(lambda agentId, cmdline, args: (
+                bof := conquest.get_string(args, 0),
+                args := conquest.get_string(args, 1),
+
+                conquest.execute_alias(agentId, cmdline, f"dll {ASYNC_DLL} {EXPORT_FUNC} {conquest.async_bof_pack(bof, args)}") if os.path.exists(bof)
+                else conquest.error(agentId, f"Failed to open object file: {bof}", cmdline)
+            ))
+).registerToGroup("execution")
