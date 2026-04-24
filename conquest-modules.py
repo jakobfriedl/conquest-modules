5ec1b01e942f23f917c925b2dffee7a9c54069e3
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2026-04-24 11:32:50 +0200

    Added all-in-one module.

---
+++ conquest-modules.py
@@ -0,0 +1,18 @@
+import conquest
+import os.path
+
+scripts = [
+    "situational-awareness/situational-awareness.py",
+    "remote-operations/remote-ops.py",
+    "privilege-escalation/privilege-escalation.py",
+    "lateral-movement/scshell.py",
+    "credential-dumping/credential-dumping.py",
+    "kerbeus/kerbeus.py",
+    "execution/no-consolation.py",
+    "execution/async-bof.py"
+]
+
+for script in scripts:
+    conquest.load_script(os.path.join(conquest.modules_root(), script))
+
+conquest.unload_script(__file__)
